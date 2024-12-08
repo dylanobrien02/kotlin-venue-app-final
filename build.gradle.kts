b43fe7afb8be9f460ac24071f82b32dbccd16fb7
dylanobrien02 <20094139@mail.wit.ie>
2024-12-08 21:58:17 +0000

    Final App with ReadMe File

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,19 +9,18 @@ group = "ie.setu"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral() // Use Maven Central to fetch dependencies
+    mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib")) // Add Kotlin standard library
+    implementation(kotlin("stdlib"))
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
     implementation("org.slf4j:slf4j-simple:2.0.16")
 
-    //For Streaming to XML and JSON
+
     implementation("com.thoughtworks.xstream:xstream:1.4.18")
     implementation("org.codehaus.jettison:jettison:1.4.1")
 
-    // For generating a Dokka Site from KDoc
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
 }
 
@@ -30,5 +29,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(16) // Ensure your JVM toolchain is set correctly
+    jvmToolchain(16)
 }
